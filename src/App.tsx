119dc7efa272b6f960078c2ae64dbde709ff8ef8
Mateo Cisneros <mcisneroserazo@gmail.com>
2026-05-26 19:54:47 -0500

    hello there

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <img src="https://www.google.com/url?sa=t&source=web&rct=j&url=https%3A%2F%2Fgiphy.com%2Fexplore%2Fwhy-hello-there&ved=0CBUQjRxqFwoTCMjs_eqg2JQDFQAAAAAdAAAAABAG&opi=89978449"></img>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
